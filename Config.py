d064d4e2cd926f9b750d1b2ded7bf46af759f0d3
Elohim09 <126894037+Elohim09@users.noreply.github.com>
2023-03-04 00:13:04 +0800

    Update Config.py

--- Config.py
+++ Config.py
@@ -3,8 +3,8 @@ import os
 class Config(object):
     API_ID = int(os.environ.get("API_ID", "6213538"))
     API_HASH = os.environ.get("API_HASH", "8ce3522bd21cc937eee4c68813d501d5")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", 6088962718:AAFqdoJFm-egJzzx_XEVFflKPqOl3EfixqM)
-    STRING_SESSION = os.environ.get("STRING_SESSION", 1BVtsOHUBuxa7f_WCpIjYn2dL8_9Km9dNM3nTLXownIza9Bs2J5ntDtmkctkSWvpc5oSZFkAKVg9XpSsxFhP4CLXEUXuakiGHn_H2lCL2WxiAz7LFKjcZv8e6aT4YqERWt6UBk7k6-5OiuHL19rSLdaouGmmyPxw1N72T-krIQ53lWbLvNRIwfMH_1IurEao1ZaCVbe7lycz5RVafEytAAWNe_PnaUq5rurwE2G5ym40fUPMDgqT71nfe_zcuTsTGgtecBMXL2KBVvKVpD5eiXszQSsdxHsaF1Ye5abm_C2ZPKM0VSBh2vtCkF82DMfkPyJ4hk_SmdUH_l-6kaZ7cb45nghUdlqI=)
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6088962718:AAFqdoJFm-egJzzx_XEVFflKPqOl3EfixqM")
+    STRING_SESSION = os.environ.get("STRING_SESSION", "1BVtsOHUBuxa7f_WCpIjYn2dL8_9Km9dNM3nTLXownIza9Bs2J5ntDtmkctkSWvpc5oSZFkAKVg9XpSsxFhP4CLXEUXuakiGHn_H2lCL2WxiAz7LFKjcZv8e6aT4YqERWt6UBk7k6-5OiuHL19rSLdaouGmmyPxw1N72T-krIQ53lWbLvNRIwfMH_1IurEao1ZaCVbe7lycz5RVafEytAAWNe_PnaUq5rurwE2G5ym40fUPMDgqT71nfe_zcuTsTGgtecBMXL2KBVvKVpD5eiXszQSsdxHsaF1Ye5abm_C2ZPKM0VSBh2vtCkF82DMfkPyJ4hk_SmdUH_l-6kaZ7cb45nghUdlqI=")
     MANAGEMENT_MODE = os.environ.get("MANAGEMENT_MODE", None)
     HEROKU_MODE = os.environ.get("HEROKU_MODE", None)
     BOT_USERNAME = os.environ.get("BOT_USERNAME", "")
